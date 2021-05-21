71743def8bf54e5e43e0b18a6d80aa6207acff9c
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro.local>
2021-05-20 18:57:16 -0700

    app made

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef,FunctionComponent } from "react";
 import { createPortal } from "react-dom";
 
 const modalRoot = document.getElementById("modal");
 
-const Modal = ({ children }) => {
+const Modal:FunctionComponent = ({ children }) => {
   const elRef = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
